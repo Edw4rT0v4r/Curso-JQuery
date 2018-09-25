a167b06bb4f28605694a2b1a51c09ad79dcbf1c1
ETOVAR <etovar@optimeconsulting.com>
2018-09-25 17:22:25 -0500

    Plugins

---
+++ lib/ts/clase70.ts
@@ -0,0 +1,14 @@
+function saludar(nombre: string) {
+    console.log('Hola ' + nombre);
+}
+
+var persona = {
+    nombre: "Edwar",
+    edad: 21,
+    sexo: "M",
+    direccion: {}
+};
+
+// var persona = "Edwar";
+
+saludar(persona.nombre);
